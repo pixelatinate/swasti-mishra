a15cee4b408ecffa00c8dd26ec7545cf65a15e2f
Swasti Mishra <swastiy2k1@gmail.com>
2021-01-03 20:00:48 -0600

    Completed tutorial!
    Finished the tutorial. I now have a skeleton for my website.

---
+++ practice/app1.js
@@ -0,0 +1,18 @@
+'use strict'
+
+const switcher = document.querySelector('.btn');
+window.onload=function(){
+    
+switcher.addEventListener('click', function() {
+    document.body.classList.toggle('dark-theme')
+
+    var className = document.body.className;
+    if(className == "light-theme") {
+        this.textContent = "Dark";
+    }
+    else {
+        this.textContent = "Light";
+    }
+
+});
+}
